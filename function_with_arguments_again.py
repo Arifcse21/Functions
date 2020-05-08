2173dcf6de0aad175e18661354722a65ef55650e
ARIF <lone_wolf@ASUS.localdomain>
2020-05-08 16:19:49 +0600

    another example of fuction with arguments

---
+++ function_with_arguments_again.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+def fun(a,b):
+        print("a=",a)
+        print("b=",b)
+
+fun(a=3,b=4)
+fun(b=6,a=7)
+#fun(a=9,c=0) # here c is not acceptable
+
+def add(a,b):
+    return  a+b
+
+total=add(4,6)
+print(total)
+
+
